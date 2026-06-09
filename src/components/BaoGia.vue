1d567e2b366144208dc17303cdc114e422290845
Kezin84 <nguyenvodinhanh@gmail.com>
2026-06-09 16:51:53 +0700

    fix: hide xuất ảnh button in trang baogia

--- src/components/BaoGia.vue
+++ src/components/BaoGia.vue
@@ -6630,7 +6630,7 @@ onUnmounted(() => {
                 <svg class="export-vip-btn-arrow" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
               </button>
 
-              <button class="export-vip-btn export-vip-btn-image" @click="showExportInfoModal = false; openImageKitModal()">
+              <button v-if="false" class="export-vip-btn export-vip-btn-image" @click="showExportInfoModal = false; openImageKitModal()">
                 <div class="export-vip-btn-icon">
                   <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"/><circle cx="8.5" cy="8.5" r="1.5"/><polyline points="21 15 16 10 5 21"/></svg>
                 </div>
